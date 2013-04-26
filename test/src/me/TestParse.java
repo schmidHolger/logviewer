f0c16b3042c0fe01022da99908755cff68448681
schmidHolger <schmid_holger@hotmail.com>
2013-04-26 22:41:48 +0200

    Add exception for tests

--- test/src/me/TestParse.java
+++ test/src/me/TestParse.java
@@ -10,10 +10,20 @@ public class TestParse {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String testString = new String("26.04.2013 08:15:23.123 [main] INFO com.me.this.Package - message for you");
+		String[] testStrings = new String[] { //
+		//
+				new String("26.04.2013 08:16:23.123 [main] INFO com.me.this.Package - message1 for you"), //
+				new String("26.04.2013 08:17:23.123 [main] INFO com.me.this.Package - message2 for you"), //
+				new String("26.04.2013 08:18:23.123 [main] INFO com.me.this.Package - message3 for you"), //
+				new String("26.04.2013 08:19:23.123 [main] INFO com.me.this.Package - message4 for you"), //
+				new String("Exception in thread \"main\" java.lang.NullPointerException"), //
+				new String("	at me.parsers.ParserEngine.parseLine(ParserEngine.java:22)"), //
+				new String("	at me.TestParse.main(TestParse.java:23)"), //
+		};
 		ParserEngine engine = new ParserEngine(PATTERN);
-		MessageParts msgParts = engine.parseLine(testString);
-		System.out.println(msgParts.toString());
+		for (String string : testStrings) {
+			MessageParts msgParts = engine.parseLine(string);
+			System.out.println(msgParts.toString());
+		}
 	}
-
 }
